3f29dbd797028c1dbfa1ef757f468378b3a6e544
Quan Ming Vang <mingvang2003@gmail.com>
2025-04-02 18:41:36 +0100

    Added operate function to return calculation

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function add(x, y) { return x + y; }
 function subtract(x, y) { return x - y; }
 function divide(x, y) { return x / y; }
 function multiply(x, y) { return x * y; }
+
+function operate(x, y, op) {
+
+  switch (op) {
+    case '+':
+      return add(x, y);
+    case '-':
+      return subtract(x, y);
+    case '/':
+      return divide(x, y);
+    case '*':
+      return multiply(x, y);
+  }
+}
+
+let x = 10;
+let y = 25;
+let op = '*';
+
+console.log(operate(x, y, op));
+
+
+
